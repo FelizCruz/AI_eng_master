8e24ff285a5a019d31097677c896874fa6996be8
FelizCruz <FelizCruz@users.noreply.github.com>
2025-11-16 18:08:21 +0400

    Update analyze-video.html with Video Indexer embedded player and insights

--- 16-video-indexer/get-videos.ps1
+++ 16-video-indexer/get-videos.ps1
@@ -1,6 +1,6 @@
-$account_id="YOUR_ACCOUNT_ID"
-$api_key="YOUR_API_KEY"
-$location="trial"
+$account_id="256e5542-d5fd-46f1-b3cc-ab29ec375613"
+$api_key=6GYUXtMtDmcySWEkzmBWozq7MWGCq20OvMv9E9LEyA7oF1TyMQaIJQQJ99BKACYeBjFXJ3w3AAAEACOGlFKC
+$location=eastus
 
 # Call the AccessToken method with the API key in the header to get an access token
 $token = Invoke-RestMethod -Method "Get" -Uri "https://api.videoindexer.ai/auth/$location/Accounts/$account_id/AccessToken" -Headers @{'Ocp-Apim-Subscription-Key' = $api_key}
